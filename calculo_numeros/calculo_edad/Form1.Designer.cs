0cd8cfc8d84aa900b36c103a49a7911923c8e1b7
michael escobar galeano <maicolgemelo@gmail.com>
2019-09-06 19:08:00 -0500

    programa que calcula el numero mayor y menor

---
+++ calculo_numeros/calculo_edad/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace calculo_edad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.txtnumero1 = new System.Windows.Forms.TextBox();
+            this.lblnumero1 = new System.Windows.Forms.Label();
+            this.lblmayor = new System.Windows.Forms.Label();
+            this.lblmay = new System.Windows.Forms.Label();
+            this.lblnumero2 = new System.Windows.Forms.Label();
+            this.txtnumero2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblmen = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitulo.Location = new System.Drawing.Point(272, 33);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(195, 24);
+            this.lbltitulo.TabIndex = 0;
+            this.lbltitulo.Text = "calculo de numeros";
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(467, 159);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(81, 25);
+            this.btncalcular.TabIndex = 1;
+            this.btncalcular.Text = "calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // txtnumero1
+            // 
+            this.txtnumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnumero1.Location = new System.Drawing.Point(331, 125);
+            this.txtnumero1.Name = "txtnumero1";
+            this.txtnumero1.Size = new System.Drawing.Size(100, 22);
+            this.txtnumero1.TabIndex = 2;
+            // 
+            // lblnumero1
+            // 
+            this.lblnumero1.AutoSize = true;
+            this.lblnumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnumero1.Location = new System.Drawing.Point(163, 125);
+            this.lblnumero1.Name = "lblnumero1";
+            this.lblnumero1.Size = new System.Drawing.Size(158, 18);
+            this.lblnumero1.TabIndex = 3;
+            this.lblnumero1.Text = "ingrese un numero :";
+            // 
+            // lblmayor
+            // 
+            this.lblmayor.AutoSize = true;
+            this.lblmayor.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmayor.Location = new System.Drawing.Point(213, 247);
+            this.lblmayor.Name = "lblmayor";
+            this.lblmayor.Size = new System.Drawing.Size(65, 18);
+            this.lblmayor.TabIndex = 4;
+            this.lblmayor.Text = "Mayor :";
+            // 
+            // lblmay
+            // 
+            this.lblmay.AutoSize = true;
+            this.lblmay.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmay.Location = new System.Drawing.Point(340, 247);
+            this.lblmay.Name = "lblmay";
+            this.lblmay.Size = new System.Drawing.Size(39, 18);
+            this.lblmay.TabIndex = 6;
+            this.lblmay.Text = "May";
+            this.lblmay.Visible = false;
+            // 
+            // lblnumero2
+            // 
+            this.lblnumero2.AutoSize = true;
+            this.lblnumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnumero2.Location = new System.Drawing.Point(163, 180);
+            this.lblnumero2.Name = "lblnumero2";
+            this.lblnumero2.Size = new System.Drawing.Size(158, 18);
+            this.lblnumero2.TabIndex = 7;
+            this.lblnumero2.Text = "ingrese un numero :";
+            // 
+            // txtnumero2
+            // 
+            this.txtnumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnumero2.Location = new System.Drawing.Point(331, 180);
+            this.txtnumero2.Name = "txtnumero2";
+            this.txtnumero2.Size = new System.Drawing.Size(100, 22);
+            this.txtnumero2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(213, 300);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 18);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Menor :";
+            // 
+            // lblmen
+            // 
+            this.lblmen.AutoSize = true;
+            this.lblmen.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmen.Location = new System.Drawing.Point(340, 300);
+            this.lblmen.Name = "lblmen";
+            this.lblmen.Size = new System.Drawing.Size(40, 18);
+            this.lblmen.TabIndex = 10;
+            this.lblmen.Text = "Men";
+            this.lblmen.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblmen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtnumero2);
+            this.Controls.Add(this.lblnumero2);
+            this.Controls.Add(this.lblmay);
+            this.Controls.Add(this.lblmayor);
+            this.Controls.Add(this.lblnumero1);
+            this.Controls.Add(this.txtnumero1);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.lbltitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.TextBox txtnumero1;
+        private System.Windows.Forms.Label lblnumero1;
+        private System.Windows.Forms.Label lblmayor;
+        private System.Windows.Forms.Label lblmay;
+        private System.Windows.Forms.Label lblnumero2;
+        private System.Windows.Forms.TextBox txtnumero2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblmen;
+    }
+}
+
---
+++ calculo_numeros/calculo_edad/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculo_edad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+
+            string num1 = txtnumero1.Text;
+            string num2 = txtnumero2.Text;
+
+            int numero1 = int.Parse(num1);
+            int numero2 = int.Parse(num2);
+
+            if (numero1 > numero2) {
+
+                lblmay.Text = numero1.ToString();
+                lblmen.Text = numero2.ToString();
+                lblmay.Visible = true;
+                lblmen.Visible = true;
+
+
+            }else{
+                if (numero2 > numero1) {
+
+
+                    lblmay.Text = numero2.ToString();
+                    lblmen.Text = numero1.ToString();
+                    lblmay.Visible = true;
+                    lblmen.Visible = true;
+
+
+                }else{
+
+                    lblmay.Text = " Son iguales";
+                    lblmen.Text = " Son iguales ";
+                    lblmay.Visible = true;
+                    lblmen.Visible = true;
+
+                }
+
+
+
+
+            }
+
+        }
+    }
+}
